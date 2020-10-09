b6a2e673c32ec17464b1a78e9501bb7086a8db52
= <developer@cluebixsoftware.com>
2020-10-09 18:22:20 +0530

    Initial commit

---
+++ application/views/backend/employees.php
@@ -0,0 +1,79 @@
+<?php $this->load->view('backend/header'); ?>
+<?php $this->load->view('backend/sidebar'); ?>
+         <div class="page-wrapper">
+            <!-- ============================================================== -->
+            <!-- Bread crumb and right sidebar toggle -->
+            <!-- ============================================================== -->
+            <div class="row page-titles">
+                <div class="col-md-5 align-self-center">
+                    <h3 class="text-themecolor"><i class="fa fa-users" aria-hidden="true"></i> Employees</h3>
+                </div>
+                <div class="col-md-7 align-self-center">
+                    <ol class="breadcrumb">
+                        <li class="breadcrumb-item"><a href="javascript:void(0)">Home</a></li>
+                        <li class="breadcrumb-item active">Employees</li>
+                    </ol>
+                </div>
+            </div>
+            <div class="message"></div>
+            <div class="container-fluid">
+                <div class="row mt-3">
+                    <div class="col-12">
+                        <div class="card card-outline-info">
+                            <div class="card-header d-flex">
+								<h4 class="m-b-0 text-white"><i class="fa fa-user-o" aria-hidden="true"></i> Employee List</h4>
+								<a href="<?php echo base_url(); ?>employee/Add_employee" class="text-white btn btn-sm btn-success ml-auto float-right"><i class="fa fa-plus"></i> Add New Employee</a>
+                            </div>
+                            <div class="card-body">
+                                <div class="table-responsive ">
+                                    <table id="employees123" class="display nowrap table table-hover table-striped table-bordered" cellspacing="0" width="100%">
+                                        <thead>
+                                            <tr>
+                                                <th>Employee Name</th>
+                                                <th>PIN</th>
+                                                <th>Email </th>
+                                                <th>Contact </th>
+                                                <th>User Type</th>
+                                                <th>Action</th>
+                                            </tr>
+                                        </thead>
+                                        <tfoot>
+                                               <tr>
+                                                <th>Employee Name</th>
+                                                <th>PIN</th>
+                                                <th>Email </th>
+                                                <th>Contact </th>
+                                                <th>User Type</th>
+                                                <th>Action</th>
+                                            </tr>
+                                        </tfoot>
+                                        <tbody>
+                                           <?php foreach($employee as $value): ?>
+                                            <tr>
+                                                <td title="<?php echo $value->first_name .' '.$value->last_name; ?>"><?php echo $value->first_name .' '.$value->last_name; ?></td>
+                                                                                <td><?php echo $value->em_code; ?></td>
+                                                <td><?php echo $value->em_email; ?></td>
+                                                <td><?php echo $value->em_phone; ?></td>
+                                                <td><?php echo $value->em_role; ?></td>
+                                                <td class="jsgrid-align-center ">
+                                                    <a href="<?php echo base_url(); ?>employee/view?I=<?php echo base64_encode($value->em_id); ?>" title="Edit" class="btn btn-sm btn-info waves-effect waves-light"><i class="fa fa-pencil-square-o"></i></a>
+                                                </td>
+                                            </tr>
+                                            <?php endforeach; ?>
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+<?php $this->load->view('backend/footer'); ?>
+<script>
+    $('#employees123').DataTable({
+        "aaSorting": [[1,'asc']],
+        dom: 'Bfrtip',
+        buttons: [
+            'copy', 'csv', 'excel', 'pdf', 'print'
+        ]
+    });
+</script>
